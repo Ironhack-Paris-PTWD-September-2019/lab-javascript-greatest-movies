5d05240b70317b5d58b89ce83894abce70eaef47
Elisa Salamanca <e_l_salamanca@yahoo.fr>
2019-10-02 21:41:43 +0200

    Iteration 6

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -41,10 +41,35 @@ function howManyMovies(tabMovies){
 };
 
 // Iteration 5: Alphabetic Order - Order by title and print the first 20 titles
-
+function orderAlphabetically(tabMovies){
+  var tabTitlesSorted=tabMovies.map(el=>el.title).sort(); 
+  if(tabTitlesSorted.length<=20){return tabTitlesSorted}; 
+  return tabTitlesSorted.slice(0,20);
+}
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
-/*var hmin=Number(movie.duration.split(" ")[0].split("h")[0])*60
-var min= Number(movie.duration.split(" ")[1].split("m")[0])
-var duration= hmin+min*/
+function turnHoursToMinutes(tabMovies){
+  var tabModified=tabMovies.map(movie=> {
+    return {
+      ...movie,
+      duration:calculateMinutes(movie)
+    }
+  }); 
+  return tabModified; 
+
+}
+
+function calculateMinutes(movie){
+  if(movie.duration.includes("h")&&movie.duration.includes("min")){
+  var hmin=Number(movie.duration.split(" ")[0].split("h")[0])*60
+  var min= Number(movie.duration.split(" ")[1].split("m")[0])
+  var duration= hmin+min
+  }else if (movie.duration.includes("h")&&!movie.duration.includes("min")){
+    duration=Number(movie.duration.split("h")[0])*60;
+  }else if (!movie.duration.includes("h")&&movie.duration.includes("min")){
+    duration=Number(movie.duration.split("m")[0])
+  }
+  return duration;
+}
+
 
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
