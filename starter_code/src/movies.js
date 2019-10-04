26318853fbf251a6be6d0475b899c45765a12a33
Sofy-Prd <sofy@DESKTOP-4LRCARK.localdomain>
2019-10-04 16:45:10 +0200

    jusqu'a l'iteration 6

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -3,6 +3,7 @@
 // Iteration 1: All rates average - Get the average of all rates with 2 decimals 
 
 function ratesAverage(movies) {
+
     var totalReview=movies.reduce((sum,elem)=>{
     return sum =sum + Number(elem.rate);
     },0);
@@ -12,7 +13,7 @@ function ratesAverage(movies) {
   
     return average;
    
-  }
+}
  
 // Iteration 2: Drama movies - Get the average of Drama Movies
 
@@ -26,14 +27,34 @@ function dramaMoviesRate(movies){
    
    if (newTab.length<1) {
        return 0;}
+
    var dramaAverage = ratesAverage(newTab);
    return dramaAverage;
-   }
+}
   
 // Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
+
+
  function orderByDuration (movies){
+
+    
      var tabWithMinutes = movies.map(function(el){ 
-         return Number(el.duration[0])*60+Number(el.duration.substring(1));
+
+        var part1= el.duration.split('h');
+        var part2= part1[1].split('min');
+        var dureeMinut=Number(part1[0])*60+Number(part2[0]);
+
+        return {
+            
+                title: el.title,
+                year: el.year,
+                director: el.director,
+                duration: dureeMinut,
+                genre: el.genre,
+                rate: el.rate
+              
+        }
+                
      })
 
      var tabInOrderDuration = tabWithMinutes.sort(function(a,b){
@@ -45,21 +66,67 @@ function dramaMoviesRate(movies){
 // Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
 
 function howManyMovies (movies) {
-    var spielbergMovies = movies.filter(function(el){
-        el.director==='Steven Spielberg';
+    var spielbergMovies;
+    var spielbergDramaMovies;
+    var numberMovies;
+    
+    spielbergMovies = movies.filter(function(el){
+        return el.director==='Steven Spielberg';
+       
     });
 
-    var spielbergDramaMovies = spielbergMovies.filter (function(el1){
-
+    spielbergDramaMovies = spielbergMovies.filter(function(el1){
+        
         return el1.genre.includes("Drama");
+        
 
     });
 
     var numberMovies = spielbergDramaMovies.length;
+    return numberMovies;
 }
 
 // Iteration 5: Alphabetic Order - Order by title and print the first 20 titles
 
+function orderAlphabetically(movies) {
+    var tabTitle=[];
+    tabTitle=movies.map(function(el){
+        return el.title;
+    })
+    var tabInOrderAlpha = tabTitle.sort();
+    var tabInOrderAlpha20=tabInOrderAlpha.splice(0,20);
+    return tabInOrderAlpha20
+
+    }
+
+
 // Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
+function turnHoursToMinutes(movies){
+
+       
+    var tabWithMinutes = movies.map(function(el){ 
+
+        var part1= el.duration.split('h');
+        var part2= part1[1].split('min');
+        var dureeMinut=Number(part1[0])*60+Number(part2[0]);
+
+        return {
+            
+                title: el.title,
+                year: el.year,
+                director: el.director,
+                duration: dureeMinut,
+                genre: el.genre,
+                rate: el.rate
+              
+        }
+                
+     })
+     
+     return tabWithMinutes;
+
+ }
+
+    
 // BONUS Iteration: Best yearly rate average - Best yearly rate average
